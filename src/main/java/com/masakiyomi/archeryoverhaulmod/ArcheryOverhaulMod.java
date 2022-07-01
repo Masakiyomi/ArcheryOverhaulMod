2a6057140bc4da57ddcff99d5660a9b37da70d05
Masakiyomi <masakiyoomi@gmail.com>
2022-07-01 18:18:06 +0200

    Initial commit

---
+++ src/main/java/com/masakiyomi/archeryoverhaulmod/ArcheryOverhaulMod.java
@@ -0,0 +1,12 @@
+package com.masakiyomi.archeryoverhaulmod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ArcheryOverhaulMod.MODID)
+public class ArcheryOverhaulMod {
+    public static final String MODID = "archeryoverhaulmod";
+
+    public ArcheryOverhaulMod(){
+
+    }
+}
